01f3ccf5687fb4f6a88c62bc6e43b46a29a51b13
crb98 <conniebusby98@gmail.com>
2025-11-29 14:46:38 +0000

    added about_us page test and widget test

---
+++ test/about_us_page_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/about_us_page.dart';
+
+void main() {
+  testWidgets('AboutUsPage shows heading and content', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: AboutUsPage()));
+    await tester.pumpAndSettle();
+
+    expect(find.text('About Us'), findsOneWidget);
+    expect(find.textContaining('Welcome to the Union Shop'), findsOneWidget);
+  });
+}
