137115d689674a4173e4b6108a49df725d58db6c
crb98 <conniebusby98@gmail.com>
2025-11-29 14:42:33 +0000

    added collections_page_test and widget tests

---
+++ test/collections_page_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:union_shop/collections_page.dart';
+
+void main() {
+  testWidgets('CollectionsPage displays header and grid', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: CollectionsPage()));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Collections'), findsWidgets);
+    expect(find.byType(GridView), findsOneWidget);
+  });
+}
